2600f4a71f2489ee7d6be003d35d2953162ce027
Julian Rüth <julian.rueth@fsfe.org>
2019-10-06 12:42:48 +0200

    Take full control of cmake arguments

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,11 @@
 # Much of this file (and the entire recipe in fact) has been taken from the
 # root-feedstock.
 
-export STDCXX=17
 export VERBOSE=1
+# Build a C++17 aware Cling
+export STDCXX=17
+# Do not perform auto-detection of CPU features
+export EXTRA_CLING_ARGS=-O2
 
 # Manually set the deployment_target
 # May not be very important but nice to do
@@ -42,11 +45,15 @@ export CXXFLAGS
 sed -i -E 's#(ROOT_TEST_DRIVER RootTestDriver.cmake PATHS \$\{THISDIR\} \$\{CMAKE_MODULE_PATH\} NO_DEFAULT_PATH)#\1 CMAKE_FIND_ROOT_PATH_BOTH#g' \
     src/cmake/modules/RootNewMacros.cmake
 
-export CMAKE_ROOT_FLAGS=${CMAKE_PLATFORM_FLAGS[@]}
+export CMAKE_CLING_ARGS=${CMAKE_PLATFORM_FLAGS[@]}
 
-# Some flags that root-feedstock sets. They probably don't hurt when building cppyy (probably much of this is already covered by the -Dminimal=ON that setup.py sets...):
-CMAKE_ROOT_FLAGS="${CMAKE_ROOT_FLAGS} -DCMAKE_INSTALL_RPATH=\"${SP_DIR}/cppyy_backend/lib\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_SKIP_BUILD_RPATH=OFF -DCLING_BUILD_PLUGINS=OFF -DPYTHON_EXECUTABLE=\"${PYTHON}\" -DTBB_ROOT_DIR=\"${PREFIX}\" -Dexplicitlink=ON -Dexceptions=ON -Dfail-on-missing=ON -Dgnuinstall=OFF -Dshared=ON -Dsoversion=ON -Dbuiltin-glew=OFF -Dbuiltin_xrootd=OFF -Dbuiltin_davix=OFF -Dbuiltin_afterimage=OFF -Drpath=ON -DCMAKE_CXX_STANDARD=17 -Dcastor=off -Dgfal=OFF -Dmysql=OFF -Doracle=OFF -Dpgsql=OFF -Dpythia6=OFF -Droottest=OFF"
+# Some flags that root-feedstock sets. They probably don't hurt when building cppyy…
+export CMAKE_CLING_ARGS="${CMAKE_CLING_ARGS} -DCMAKE_INSTALL_RPATH=\"${SP_DIR}/cppyy_backend/lib\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_SKIP_BUILD_RPATH=OFF -DCLING_BUILD_PLUGINS=OFF -DPYTHON_EXECUTABLE=\"${PYTHON}\" -DTBB_ROOT_DIR=\"${PREFIX}\" -DCMAKE_INSTALL_PREFIX=\"${PREFIX}\" -Dexplicitlink=ON -Dexceptions=ON -Dfail-on-missing=ON -Dgnuinstall=OFF -Dshared=ON -Dsoversion=ON -Dbuiltin-glew=OFF -Dbuiltin_xrootd=OFF -Dbuiltin_davix=OFF -Dbuiltin_afterimage=OFF -Drpath=ON -DCMAKE_CXX_STANDARD=17 -Dcastor=off -Dgfal=OFF -Dmysql=OFF -Doracle=OFF -Dpgsql=OFF -Dpythia6=OFF -Droottest=OFF"
+# Variables that cppyy's setup.py usually sets, we might not actually want all of this 
+export CMAKE_CLING_ARGS="${CMAKE_CLING_ARGS} -DCMAKE_CXX_STANDARD=${STDCXX} -DLLVM_ENABLE_TERMINFO=0 -Dminimal=ON -Dasimage=OFF -Droot7=OFF -Dhttp=OFF -Dbuiltin_pcre=ON -Dbuiltin_freetype=OFF -Dbuiltin_zlib=ON -Dbuiltin_xxhash=ON"
 # Use conda-forge's clang & llvm
-CMAKE_ROOT_FLAGS="${CMAKE_ROOT_FLAGS} -Dbuiltin_llvm=OFF -Dbuiltin_clang=OFF"
+export CMAKE_CLING_ARGS="${CMAKE_CLING_ARGS} -Dbuiltin_llvm=OFF -Dbuiltin_clang=OFF"
+# Use cmake to build
+unset MAKE
 
 python -m pip install . --no-deps -vv
